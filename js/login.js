01c355f6a351ade5e3f1afbb55fff161071455d4
ALMojahid <your-al.1994mj@gmail.com>
2026-09-10 21:58:09 +0600

    Login Form Elements

---
+++ js/login.js
@@ -0,0 +1,9 @@
+// Login Form Elements
+
+const loginForm = document.getElementById("loginForm");
+
+const usernameInput = document.getElementById("username");
+
+const passwordInput = document.getElementById("password");
+
+const loginError = document.getElementById("loginError");
